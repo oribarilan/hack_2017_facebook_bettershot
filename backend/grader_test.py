c68dd91beeb114a907786fef1c7ab26bd941577d
Liran Vaknin <liranvaknin@gmail.com>
2016-09-02 15:21:18 +0300

    UBER CLEANUP

--- backend/grader_test.py
+++
@@ -1,9 +0,0 @@
-from imagegrader import LandscapeGrader
-
-test_exif = {"iso": 200,
-        "f_stop": 30,
-        "focal_length": 14,
-        "shutter_speed": 10}
-
-result = LandscapeGrader().grade(test_exif)
-print(result)
